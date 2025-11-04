706c1bbea1ee2082be8eaa490b1ea967de44884f
Xiao Cheng <xcheng@kinaxis.com>
2025-11-04 18:33:52 +0100

    reverse linked list two ways && can also be done with recursion

---
+++ src/main/java/com/xiao/algorithms/neetcode/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,89 @@
+package com.xiao.algorithms.neetcode.linkedlist;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ReverseLinkedList {
+
+
+	// space complexity O(N), time complexity O(N)
+	public ListNode reverseList(ListNode head) {
+		// use a stack
+		Deque<ListNode> stack = new ArrayDeque<>();
+
+		if (head == null) return null;
+
+		while (head.next != null) {
+			stack.push(head);
+			head = head.next;
+		}
+
+		// now the head is the end node
+		// newHead and head are two separate variables that initially point to the same object
+		// As head is used to transverse down the stack, it does not impact where newHead is pointing to.
+		ListNode newHead = head;
+
+		while (!stack.isEmpty()) {
+			ListNode nextNode = stack.pop();
+			head.next = nextNode;
+			head = nextNode;
+		}
+		// now the head is at the end again, set its next to null
+		head.next = null;
+
+		return newHead;
+	}
+
+	// Space complexity O(1), time O(N)
+	public ListNode reverseListInSpace(ListNode head) {
+		ListNode prev = null;
+		ListNode curr = head;
+		ListNode temp;
+
+		while ( curr != null) {
+			// store where next is before it gets updated with the reverse below
+			temp = curr.next;
+			curr.next = prev;
+
+			prev = curr;
+			curr = temp;
+		}
+		// quick run down given an array of [2, 3], append null before and after then we have
+		// null -> 2 -> 3 -> null
+		// before iteration, prev = null, curr = 2
+		// after first iteration, temp = 3, prev = 2, current = 3 (and link between null and 2 are reversed)
+		// so null <- 2 -> 3 -> null
+		// second iteration, temp = null, prev = 3, curr = null (and link between 2 and 3 are reversed)
+		// we can see that it ends when curr = null and our end node is pointed by prev
+		return prev;
+	}
+
+	public static void  main(String[] args) {
+		ListNode first = new ListNode(0);
+		ListNode second = new ListNode(1);
+		ListNode third = new ListNode(2);
+		ListNode fourth = new ListNode(3);
+
+		first.next = second;
+		second.next = third;
+		third.next = fourth;
+
+		prettyPrint(first);
+
+		ReverseLinkedList driver = new ReverseLinkedList();
+		ListNode firstRev = driver.reverseList(first);
+		prettyPrint(firstRev);
+
+		ListNode secondRev =  driver.reverseListInSpace(firstRev);
+		prettyPrint(secondRev);
+
+	}
+
+	private static void prettyPrint(ListNode head) {
+		while(head.next != null) {
+			System.out.println(head.val);
+			head = head.next;
+		}
+		System.out.println(head.val);
+	}
+}
--- src/main/java/com/xiao/algorithms/neetcode/twopointers/IsPalindrome.java
+++ src/main/java/com/xiao/algorithms/neetcode/twopointers/IsPalindrome.java
@@ -11,6 +11,7 @@ public class IsPalindrome {
 	}
 
 
+	// space O(1), time O(n)
 	private boolean isPalindromeTwoPointers(String s) {
 		int start = 0;
 		int end  = s.length() - 1;
@@ -26,7 +27,12 @@ public class IsPalindrome {
 		return true;
 	}
 
-	// recursion, probably uses more space as more strings need to be created
+	// recursion, probably uses more space as more strings need to be created,
+	// another issue is that substring creates a brand new string by copying all the chars
+	// the first call copies n-2, the second call copies n-4 charaters. so on
+	// the total work is (n-2)+(n-4)+(n-6)...+1, this ia quadratic series n^2. A better approach is
+	// to use indices
+	// Space O(n^2), time O(n)
 	private boolean isPalindromeRecursive(String s) {
 		// even number of characters will eventually have s.length = 0.
 		if (s.length() <= 1) {
@@ -40,6 +46,19 @@ public class IsPalindrome {
 		return isPalindromeRecursive(s.substring(1, s.length()-1));
 	}
 
+	//Space O(n), new stack needs to be created for every recurisve call
+	//time O(n)
+	private boolean isPalindromeRecursiveVer2(String s, int start, int end) {
+		if (start >= end) {
+			return true;
+		}
+
+		if (s.charAt(start) != s.charAt(end)) {
+			return false;
+		}
+
+		return isPalindromeRecursiveVer2(s, start+1, end-1);
+	}
 
 	public static void main(String[] args) {
 		IsPalindrome driver = new IsPalindrome();
